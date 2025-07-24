a7b036676c5c15b16ee5a64e26dcf4dc39cdb894
Amaan <amaantanveer667@gmail.com>
2025-07-24 16:48:35 +0100

    fix: use __dirname workaround for ES modules in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
