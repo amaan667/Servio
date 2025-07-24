388790c134f99abb17d3582527bef358175c53eb
Amaan <amaantanveer667@gmail.com>
2025-07-24 18:05:11 +0100

    chore: remove Dockerfile and write-vision-key.sh for platform-native deployment

--- write-vision-key.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Write the Google Vision JSON env variable to a file
-if [ -z "$GOOGLE_VISION_JSON" ]; then
-  echo "GOOGLE_VISION_JSON is not set!"
-  exit 1
-fi
-echo "$GOOGLE_VISION_JSON" > /app/google-vision-key.json
-export GOOGLE_APPLICATION_CREDENTIALS=/app/google-vision-key.json
-npm start 
